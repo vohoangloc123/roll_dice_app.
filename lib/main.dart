8b8cf7cf14fbb73291d82e12df326d1b03986869
vohoangloc123 <you@example.com>
2024-08-09 22:50:20 +0700

    Update gradient colors and remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,13 @@ import 'gradient_container.dart';
 void main() {
   runApp(
     const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body:GradientContainer(
          colors: [
-            Color.fromARGB(255, 33, 5, 109),
-            Color.fromARGB(255, 68, 21, 149),
+            Color.fromARGB(255, 65, 88, 208),
+            Color.fromARGB(255, 200, 80, 192),
+           Color.fromARGB(255, 255, 204, 112),
           ],
         ),
       ),
